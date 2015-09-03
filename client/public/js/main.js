199681592ffc0a9cd2548d88a57373bd08abc5fe
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-09-03 16:56:02 -0600

    Finish basic ajax requests

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,11 +1,16 @@
-var token;
-var random;
-var tempArray = [];
+var token, random, langs;
 $(window).on('load',function() {
   console.log('loaded');
   getToken();
   randomWord();
+  setTimeout(languages, 200);
 });
+
+function createList(items) {
+  for (var i = 0; i < items.length; i++) {
+    $('.dropdown-menu').append('<li><a href="#">' + langs[i] + '</a></li>');
+  }
+}
 
 function randomWord() {
   $.ajax({
@@ -32,7 +37,7 @@ function translate(word, langTo) {
     jsonp: 'oncomplete',
   }).done(function(data){
     console.log(data);
-    tempArray.push(data);
+    //Boom!
   }).fail(function(error) {
     getToken();
   });
@@ -47,7 +52,8 @@ function getLanguage(string) {
       text: string
     }
   }).done(function(data) {
-    return data;
+    langs = data;
+    console.log(langs);
   }).fail(function(error) {
     return error.status;
   });
@@ -65,6 +71,21 @@ function getToken() {
     }
   });
 }
+function languages() {
+  $.ajax({
+    url: 'http://api.microsofttranslator.com/V2/Ajax.svc/GetLanguageNames',
+    data: {
+      appId: 'Bearer ' + token,
+      locale: 'en',
+      languageCodes: '["ar", "bs-Latn", "bg", "ca", "zh-CHS", "zh-CHT", "hr", "cs", "da", "nl", "en", "et", "fi", "fr", "de", "el", "ht", "he", "hi", "mww", "hu", "id", "it", "ja", "tlh", "tlh-Qaak", "ko", "lv", "lt", "ms", "mt", "yua", "no", "otq", "fa", "pl", "pt", "ro", "ru", "sr-Cyrl", "sr-Latn", "sk", "sl", "es", "sv", "th", "tr", "uk", "ur", "vi", "cy"]'
+    }
+  }).done(function(data) {
+    langs = JSON.parse(data);
+    setTimeout(createList(langs), 400);
+  }).fail(function(error) {
+    console.log(error.status);
+  });
+}
 
 function compare(userWord, correctWord) {
   if (userWord === correctWord) {
@@ -74,4 +95,37 @@ function compare(userWord, correctWord) {
     console.log('Incorrect');
     // push incorect++
   }
+}
+function validate(apiWord, userWord) {
+  var d = []; //2d matrix
+  var n = apiWord.length;
+  var m = userWord.length;
+  if (n === 0) return m;
+  if (m === 0) return n;
+  for (var i = n; i >= 0; i--) d[i] = [];
+  for (var i = n; i >= 0; i--) d[i][0] = i;
+  for (var j = m; j >= 0; j--) d[0][j] = j;
+  for (var i = 1; i <= n; i++) {
+    var apiWord_i = apiWord.charAt(i - 1);
+      for (var j = 1; j <= m; j++) {
+        if (i == j && d[i][j] > 4) return n;
+           var userWord_j = userWord.charAt(j - 1);
+           var cost = (apiWord_i == userWord_j) ? 0 : 1; // Step 5
+           var mi = d[i - 1][j] + 1;
+           var b = d[i][j - 1] + 1;
+           var c = d[i - 1][j - 1] + cost;
+           if (b < mi) mi = b;
+           if (c < mi) mi = c;
+           d[i][j] = mi; // Step 6
+           if (i > 1 && j > 1 && apiWord_i == userWord.charAt(j - 2) && apiWord.charAt(i - 2) == userWord_j) {
+               d[i][j] = Math.min(d[i][j], d[i - 2][j - 2] + cost);
+           }
+       }
+   }
+   // Step 7
+   if (d[n][m] <= 1) {
+       return true;
+     } else {
+         return false;
+   }
 }
