287b1a808fdd95a3190c6d9d1499eb0f9d18a2b0
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-09-03 14:12:12 -0600

    Add detect language function

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,17 +1,17 @@
 var token;
-
+var tempArray = [];
 $(window).on('load',function() {
   console.log('loaded');
   getToken();
 });
 
-function translate(word, langFrom, langTo) {
+function translate(word, langTo) {
   $.ajax({
     url: 'http://api.microsofttranslator.com/V2/Ajax.svc/Translate',
     dataType: 'jsonp',
     data: {
       appId: 'Bearer ' + token,
-      from: langFrom,
+      from: getLanguage(word),
       to: langTo,
       contentType: 'text/plain',
       text: word
@@ -19,9 +19,22 @@ function translate(word, langFrom, langTo) {
     jsonp: 'oncomplete',
   }).done(function(data){
     console.log(data);
+    tempArray.push(data);
   }).fail(function(error) {
     getToken();
-    // translate(word, langFrom, langTo);
+  });
+}
+
+function getLanguage(string) {
+  $.ajax({
+    url: 'http://api.microsofttranslator.com/V2/Ajax.svc/Detect',
+    method: 'GET',
+    data: {
+      appId: 'Bearer ' + token,
+      text: string
+    }
+  }).done(function(data) {
+    console.log(data);
   });
 }
 
@@ -31,10 +44,19 @@ function getToken() {
     method: 'GET',
     success: function(data) {
       token = data.token;
-      console.log(token);
     },
     error: function(error) {
       return error.status;
     }
   });
+}
+
+function compare(userWord, correctWord) {
+  if (userWord === correctWord) {
+    console.log('Correct!');
+    // push correct++
+  } else {
+    console.log('Incorrect');
+    // push incorect++
+  }
 }
