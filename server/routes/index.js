be90df04ddc4714e025b36ae14b7d0d72f20f7fc
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-09-02 20:58:07 -0600

    complete successful http request for auth token

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,28 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var ajax = require('najax');
-var http = require('http');
 
 var request = {
   url: 'https://datamarket.accesscontrol.windows.net/v2/OAuth2-13',
-  type: 'POST',
-  header: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-  },
+  method: 'POST',
+  contentType: 'application/x-www-form-urlencoded',
   data: {
-    grant_type: 'client_credentials',
     client_id: 'galvanize',
     client_secret: process.env.client_secret,
-    scope: 'http://api.microsofttranslator.com'
+    grant_type: 'client_credentials',
+    scope: 'http://api.microsofttranslator.com/'
   },
   success: function(data) {
-    console.log(data);
-    router.get('/', function(req, res, next) {
-      res.render(data);
+    data = JSON.parse(data);
+    router.get('/', function(req, res) {
+      res.json(data);
     });
   },
   error: function(error) {
     console.log('Error: ' + error.status);
+    console.log(request);
   }
 };
 
